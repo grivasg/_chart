4ecf9add66e21d397134df216d253292034e9d0b
Frankd11366 <capurivas@gmail.com>
2024-08-26 11:49:33 -0600

    ACTUALIZACION DE GRAFICAS

---
+++ views/cliente/estadisticas.php
@@ -0,0 +1,21 @@
+<h1 class="class text-center">DEPARTAMENTO DE ESTADISTICA</h1>
+
+
+<h3 class="class text-center">TOTAL DE VENTAS BY RIVAS GARCIA</h3>
+
+<div class="row justify-content-center">
+    <div class="col-lg-6">
+<button type="button" id="actualizar" class="btn btn-info w-20">Actualizar</button>
+    </div>
+</div>
+
+<div class="row justify-content-center">
+    <div class="col">
+        <canvas id="chartVentas" width="100%"></canvas>
+    </div>
+</div>
+
+
+
+<script src="<?= asset('./build/js/cliente/estadistica.js') ?>"></script>
+
