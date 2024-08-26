00d5f42ebe59a9bf9abb1bf9937b5f0dcb7fb9b0
Frankd11366 <capurivas@gmail.com>
2024-08-26 12:04:29 -0600

    NEW

--- views/cliente/estadisticas.php
+++ views/cliente/estadisticas.php
@@ -5,7 +5,7 @@
 
 <div class="row justify-content-center">
     <div class="col-lg-6">
-<button type="button" id="actualizar" class="btn btn-success w-20"> Actualizar</button>
+<button type="button" id="actualizar" class="btn btn-success w-100"> Actualizar</button>
     </div>
 </div>
 
